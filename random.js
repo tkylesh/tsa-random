2cad7aaa7602f2233ce3da3b129bc347425ba5ee
Taylor Shuff <tkylesh@gmail.com>
2016-09-01 15:19:46 -0500

    added both right and left arrows html and css, made notes on how to do the javascript file

---
+++ random.js
@@ -0,0 +1,11 @@
+//randomize arrows when page is clicked
+	//user Math.random()
+	//then assign left if random >= number
+	//or right if random is <= number
+
+/*
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+*/
